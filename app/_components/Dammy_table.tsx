843a678ebf2cdbafe21b11f9a2651373bbc64494
nashisuke <h1.konashi@gmail.com>
2024-09-27 14:11:25 +0900

    Homeページの調整
    見やすさと分かりやすさを向上させるためHomeページを変更

---
+++ app/_components/Dammy_table.tsx
@@ -0,0 +1,65 @@
+export default function Dammy_table() {
+    return (
+        <table className="table-auto border-collapse border border-gray-500">
+        <thead>
+          <tr>
+            <th className="border border-gray-500 px-4 py-2">科目番号</th>
+            <th className="border border-gray-500 px-4 py-2">科目名</th>
+            <th className="border border-gray-500 px-4 py-2">出席</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GC54301</td>
+            <td className="border border-gray-500 px-4 py-2">情報可視化</td>
+            <td className="border border-gray-500 px-4 py-2">なし</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">2234273</td>
+            <td className="border border-gray-500 px-4 py-2">応用体育　バスケットボール</td>
+            <td className="border border-gray-500 px-4 py-2">あり</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GC51901</td>
+            <td className="border border-gray-500 px-4 py-2">マークアップ言語</td>
+            <td className="border border-gray-500 px-4 py-2">なし</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GC52901</td>
+            <td className="border border-gray-500 px-4 py-2">インタラクションデザイン</td>
+            <td className="border border-gray-500 px-4 py-2">あり</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GC22201</td>
+            <td className="border border-gray-500 px-4 py-2">プログラム言語論</td>
+            <td className="border border-gray-500 px-4 py-2">なし</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GB40301</td>
+            <td className="border border-gray-500 px-4 py-2">ヒューマンインタフェース</td>
+            <td className="border border-gray-500 px-4 py-2">あり</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GE70601</td>
+            <td className="border border-gray-500 px-4 py-2">Webプログラミング</td>
+            <td className="border border-gray-500 px-4 py-2">なし</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GE70801</td>
+            <td className="border border-gray-500 px-4 py-2">データ表現と処理</td>
+            <td className="border border-gray-500 px-4 py-2">なし</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GE70501</td>
+            <td className="border border-gray-500 px-4 py-2">情報検索システム</td>
+            <td className="border border-gray-500 px-4 py-2">あり</td>
+          </tr>
+          <tr>
+            <td className="border border-gray-500 px-4 py-2">GE71001</td>
+            <td className="border border-gray-500 px-4 py-2">情報デザインとインタフェース</td>
+            <td className="border border-gray-500 px-4 py-2">あり</td>
+          </tr>
+        </tbody>
+      </table>
+    )
+}
