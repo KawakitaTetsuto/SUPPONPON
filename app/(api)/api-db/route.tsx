f9094c6e3865cb2c205b34a542d5a28281fe289a
Kawakita <s2210093@s.tsukuba.ac.jp>
2024-09-27 15:15:47 +0900

    apiルーターの設定
    VercelPostgresとNext.jsとで通信するためのAPIルーターを記述しました

---
+++ app/(api)/api-db/route.tsx
@@ -0,0 +1,29 @@
+import { sql } from '@vercel/postgres';
+import { redirect } from 'next/navigation';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET() {
+    const { rows } = await sql`SELECT * FROM review LIMIT = 5;`;
+    let class_data
+    for (let i=0; i < 5; i++){
+        class_data = await sql`SELECT name FROM class WHICH id= ${rows[i].class_id};`;
+        rows[i].class_name = class_data.rows[0].name
+    }
+
+    return NextResponse.json(rows)
+}
+
+export async function POST(request: Request) {
+    const body = await request.formData();
+    console.log(body)
+    const class_id = body.get('class_id')
+    const comment = body.get('comment')
+    const attend = body.get('attend')
+
+    try {
+        if (!class_id || !attend) throw new Error('Class id and Attend required');
+        await sql`INSERT INTO review (class_id, comment, attend) VALUES(${class_id}, ${comment}, ${attend});`;
+    } catch (error) {
+        return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+}
