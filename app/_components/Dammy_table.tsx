604cb3d2c2e47fc4e0ba4d8c8f13376017bec88d
Kawakita <s2210093@s.tsukuba.ac.jp>
2024-10-11 15:44:46 +0900

    投稿閲覧機能の追加
    ホームページ上に実際にDBに保存している投稿を表示するための機能を追加しました

--- app/_components/Dammy_table.tsx
+++ app/_components/Dammy_table.tsx
@@ -1,4 +1,9 @@
-export default function Dammy_table() {
+import { GET } from '../(api)/api-db/route';
+
+export async function Dammy_table() {
+	const reviews = await GET()
+	//console.log('%o', reviews)
+
     return (
       <div>
         <h1>ダミーテーブル</h1>
@@ -8,59 +13,18 @@ export default function Dammy_table() {
             <th className="border border-gray-500 px-4 py-2">科目番号</th>
             <th className="border border-gray-500 px-4 py-2">科目名</th>
             <th className="border border-gray-500 px-4 py-2">出席</th>
+            <th className="border border-gray-500 px-4 py-2">コメント</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GC54301</td>
-            <td className="border border-gray-500 px-4 py-2">情報可視化</td>
-            <td className="border border-gray-500 px-4 py-2">なし</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">2234273</td>
-            <td className="border border-gray-500 px-4 py-2">応用体育　バスケットボール</td>
-            <td className="border border-gray-500 px-4 py-2">あり</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GC51901</td>
-            <td className="border border-gray-500 px-4 py-2">マークアップ言語</td>
-            <td className="border border-gray-500 px-4 py-2">なし</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GC52901</td>
-            <td className="border border-gray-500 px-4 py-2">インタラクションデザイン</td>
-            <td className="border border-gray-500 px-4 py-2">あり</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GC22201</td>
-            <td className="border border-gray-500 px-4 py-2">プログラム言語論</td>
-            <td className="border border-gray-500 px-4 py-2">なし</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GB40301</td>
-            <td className="border border-gray-500 px-4 py-2">ヒューマンインタフェース</td>
-            <td className="border border-gray-500 px-4 py-2">あり</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GE70601</td>
-            <td className="border border-gray-500 px-4 py-2">Webプログラミング</td>
-            <td className="border border-gray-500 px-4 py-2">なし</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GE70801</td>
-            <td className="border border-gray-500 px-4 py-2">データ表現と処理</td>
-            <td className="border border-gray-500 px-4 py-2">なし</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GE70501</td>
-            <td className="border border-gray-500 px-4 py-2">情報検索システム</td>
-            <td className="border border-gray-500 px-4 py-2">あり</td>
-          </tr>
-          <tr>
-            <td className="border border-gray-500 px-4 py-2">GE71001</td>
-            <td className="border border-gray-500 px-4 py-2">情報デザインとインタフェース</td>
-            <td className="border border-gray-500 px-4 py-2">あり</td>
-          </tr>
+        {reviews.map((row, index) => {
+          return <tr>
+                  <td className="border border-gray-500 px-4 py-2" key={row.class_id}>{row.class_id}</td>
+                  <td className="border border-gray-500 px-4 py-2"key={row.class_name}>{row.class_name}</td>
+                  <td className="border border-gray-500 px-4 py-2"key={row.attend}>{row.attend}</td>
+                  <td className="border border-gray-500 px-4 py-2"key={row.comment}>{row.comment}</td>
+                </tr>;
+        })}
         </tbody>
       </table>
     </div>
