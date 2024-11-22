c42741830c1cb3fa7ada8ae01fd7cbc9052e81a9
TaikiIkemoto <kankerioji2@gmail.com>
2024-11-22 15:42:43 +0900

    配置の修正

--- app/reviews/[class_id]/page.tsx
+++ app/reviews/[class_id]/page.tsx
@@ -25,7 +25,7 @@ export default async function detail_page({
 	const class_data: Class[] = await response.json();
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+			<main className="flex flex-col gap-8 row-start-2 items-left sm:items-start">
 				<div>
 					<p className='text-sm mb-0 pb-0'>{class_data[0].id}</p>
 					<h1 className='text-3xl mt-0 pt-0'>{class_data[0].name}</h1>
