172a8cdcc346db049fd2948cd3c57bf3c6f11e41
Kawakita <s2210093@s.tsukuba.ac.jp>
2024-12-04 13:12:12 +0900

    クラス一覧で見れる出席の情報が正しく表示されるようにしました

--- app/(api)/api-db/search-class/route.tsx
+++ app/(api)/api-db/search-class/route.tsx
@@ -13,18 +13,23 @@ export async function GET(request: NextRequest,) {
     // 科目番号か授業名かの部分一致で授業情報を取得
     const { rows } = await sql`SELECT * FROM class WHERE id like ${keyword} OR name like ${keyword} limit 20;`;
 
+    console.log('%o', rows)
     // 出席情報の過半数がはい、いいえのどちらかなのかを取得
     let attend
+    let count
+    let sum
     for(let i=0; i<rows.length; i++) {
-        attend = await sql`SELECT COUNT(*), SUM(attend) FROM review WHERE class_id = ${rows[i].class_id};`;
-        if (attend.rows[0].sum / attend.rows[0].count < 0.5) {
+        attend = await sql`SELECT COUNT(*), SUM(attend) FROM review WHERE class_id like ${rows[i].id};`;
+        count = Number(attend.rows[0].count)
+        sum = Number(attend.rows[0].sum)
+
+        console.log('%o', attend.rows[0])
+        console.log('%o', sum)
+        if ( count === 0 || (sum/ count < 0.5)) {
             rows[i].attend_metrics = 0;
         } else {
             rows[i].attend_metrics = 1;
         }
     }
-
-    console.log('%o', ThisYear);
-    console.log('%o', rows);
     return NextResponse.json(rows)
 }
